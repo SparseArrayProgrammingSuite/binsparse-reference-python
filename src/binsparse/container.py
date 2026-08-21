14bf85df365d58f4fc88f0df2302f16d8336eead
Willow Ahrens <willow.marie.ahrens@gmail.com>
2026-08-21 00:08:34 -0400

    fix

--- src/binsparse/container.py
+++ src/binsparse/container.py
@@ -17,6 +17,22 @@ from .errors import BinsparseParseError
 BINSPARSE_HEADER = "binsparse"
 
 
+dtype_to_str = {
+    np.dtype("int8"): "int8",
+    np.dtype("int16"): "int16",
+    np.dtype("int32"): "int32",
+    np.dtype("int64"): "int64",
+    np.dtype("uint8"): "uint8",
+    np.dtype("uint16"): "uint16",
+    np.dtype("uint32"): "uint32",
+    np.dtype("uint64"): "uint64",
+    np.dtype("float32"): "float32",
+    np.dtype("float64"): "float64",
+    np.dtype("bool"): "bint8",
+}
+
+str_to_dtype = {value: key for key, value in dtype_to_str.items()}
+
 class BinsparseContainer(ABC):
     """Common interface to a Binsparse binary container or container group."""
 
